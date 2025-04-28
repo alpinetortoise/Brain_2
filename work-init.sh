a2e84f1d74da6333c858e70a08c305094ad2d926
Joshua <alpinetortoise@jpc.id.au>
2025-04-28 18:19:34 +1000

    updated script to also pull relevant submodules

--- work-init.sh
+++ work-init.sh
@@ -2,5 +2,6 @@
 
 git clone git@github.com:alpinetortoise/Brain_2.git
 cd Brain_2
-git submodules init '07 Cyber/07.01 Work'
-git submodules init '05 General/05.04 Dotfiles'
+git submodule init '07 Cyber/07.01 Work'
+git submodule init '05 General/05.04 Dotfiles'
+git pull --recurse-submodules
